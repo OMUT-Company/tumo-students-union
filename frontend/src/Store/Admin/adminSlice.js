0a6541a4eaa26be477da2773a8cbe056aee2a38a
Artur1995 <artur.araqelyan1995@gmail.com>
2022-12-17 15:01:25 +0400

    bug fixed

--- frontend/src/Store/Admin/adminSlice.js
+++ frontend/src/Store/Admin/adminSlice.js
@@ -1,5 +1,5 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
-import {adminService} from "./adminservice"
+import {adminService} from "./adminService";
 
 const initialState = {
     signIn: {
